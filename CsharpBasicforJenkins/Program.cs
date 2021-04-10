6644fb0376041faa69418c06f311b9104d564ae8
Revanth G <44274406+RevanthGk1@users.noreply.github.com>
2021-04-10 21:12:28 +0200

    Update Program.cs

--- CsharpBasicforJenkins/Program.cs
+++ CsharpBasicforJenkins/Program.cs
@@ -10,7 +10,7 @@ namespace CsharpBasicforJenkins
     {
         static void Main(string[] args)
         {
-            //sample checkin 4
+            //sample checkin 6
             Console.Write("Main-Hello");
             Console.WriteLine(createMessage());
         }
